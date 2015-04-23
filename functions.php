01904fd7fac46d66b26cd55c92c7d0a4adda6480
Alison Barrett <alison.barrett@gokartlabs.com>
2015-04-23 10:53:29 -0500

    More code style updates

--- functions.php
+++ functions.php
@@ -1,25 +1,23 @@
 <?php
 
 // LOGGING FUNCTION
-if ( !function_exists('_log') ) {
-
-  function _log( $message ) {
-    if ( WP_DEBUG === true ){
-      if ( is_array( $message ) || is_object( $message ) ) {
-        error_log( print_r( $message, true ) );
-      } else {
-        error_log( $message );
-      }
-    }
-  }
-
+if ( ! function_exists( '_log' ) ) {
+	function _log( $message ) {
+		if ( true === WP_DEBUG ) {
+			if ( is_array( $message ) || is_object( $message ) ) {
+				error_log( print_r( $message, true ) );
+			} else {
+				error_log( $message );
+			}
+		}
+	}
 }
 
 // REQUIRE CLASSES
 require_once 'includes/class-project.php';
 
-if ( is_admin() ) { 
-  require_once 'includes/class-project-admin.php';
+if ( is_admin() ) {
+	require_once 'includes/class-project-admin.php';
 }
 
 // REQUIRE EXTENSIONS
